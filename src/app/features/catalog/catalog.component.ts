fb36179ba0429b0886b8be7953d394efd5fa9ed8
Amedeo Pro <amedeopro@me.com>
2020-05-26 15:39:34 +0200

    first commit

---
+++ src/app/features/catalog/catalog.component.ts
@@ -0,0 +1,20 @@
+import  { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-catalog',
+  template: `
+    <p>
+      catalog works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
